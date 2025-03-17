1ac062f9aeb4d05b77053532a6617137b597fccd
shirocodes <ishirapt@gmail.com>
2025-03-17 11:28:41 +0300

    Polished the js: Game functional

--- Wanjiru/titac.js
+++ Wanjiru/titac.js
@@ -1,6 +1,7 @@
 // the game takes two players - the user (x) and computer (o)
 // A win is when the X's or 0's are in one row or column, otherwise, it's a draw
 
+let boxes; //storing the individual clickable boxes
 let boardArray = ['', '', '', '', '', '', '', '', ''] //empty strings on 3*3 grid
 let gameOn = true; //to stop the game after a win/draw
 let xScores = 0; //track player x' wins
@@ -14,7 +15,7 @@ const winCases = [ //Possible ways of winning
 
 //ensure doc loading, loop over boxes, add event listeners 
 document.addEventListener('DOMContentLoaded', () => {
-    const boxes = document.querySelectorAll('.box');
+    boxes = document.querySelectorAll('.box');
     const playAgain = document.getElementById('restart');
 
     boxes.forEach(box => {
@@ -28,7 +29,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 //initializing the  game
     // user end
-function playerSelection(cell, index) {       //Is the box empty and game active?
+function playerSelection(box, index) {       //Is the box empty and game active?
     if (boardArray[index] === '' && gameOn) {
         boardArray[index] = 'X';
         box.textContent = 'X';
@@ -40,14 +41,13 @@ function playerSelection(cell, index) {       //Is the box empty and game active
 function simulatedMove() {
     let emptybox = []
     for (let i = 0; i < boardArray.length; i++) {
-        if (boardArray[i] === '') {
-            emptybox.push(i);
-        }
+        if (boardArray[i] === '') emptybox.push(i);
     }
+
     if (emptybox.length > 0 && gameOn) {
         const randomBoxIndex = emptybox[Math.floor(Math.random() * emptybox.length)]
+        boxes[randomBoxIndex].textContent = 'O';
         boardArray[randomBoxIndex] = 'O'
-        document.querySelector(`.box[data-index = "${randomBoxIndex}]`).textContent = 'O';
         confirmEndedGame();
     }
 }
@@ -83,8 +83,9 @@ function checkWin(player) { //loops through the wincases to find a winner line
 //update the scores
 function updateScoringCard() {
     const winnerScores = document.querySelectorAll('#scoring-card .score span');
-    winnerScores[0] = xScores;
-    winnerScores[1] = oScores;
+
+    winnerScores[0].textContent = xScores;
+    winnerScores[1].textContent = oScores;
 }
 
 function resetGame() {
